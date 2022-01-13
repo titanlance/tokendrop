7673fd0427ce28046ec2102689c3e6090c94c015
"Lance Seidman" <"lance@titan.io">
2022-01-13 02:02:41 -0800

    Fix whitelist

--- contracts/Drop.sol
+++ contracts/Drop.sol
@@ -12,13 +12,20 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract FastLumerinDrop {
     address public owner;
     uint256 public balance;
+    address[] public addressList;
     IERC20 Lumerin = IERC20(0x9D7f74d0C41E726EC95884E0e97Fa6129e3b5E99);
 
-    mapping(address => bool) whitelisted;
-    
+    mapping(address => bool) public whitelisted;
+    mapping(address => WalletWhitelist) public walletWhitelist;
+
+    event NewWallet(address sender, address newAddress);
     event TransferReceived(address _from, uint _amount);
     event TransferSent(address _from, address _destAddr, uint _amount);
-    
+
+    struct WalletWhitelist {
+        bool theAddress;
+    }
+
     constructor() {
         owner = msg.sender;
     }
@@ -33,15 +40,20 @@ contract FastLumerinDrop {
         emit TransferReceived(msg.sender, msg.value);
     }    
 
+    function getWalletCount() public view returns(uint count) {
+        return addressList.length;
+    }
+    function addWalletAddress(address newAddress) public returns(bool success) {
+        addressList.push(newAddress);
+        walletWhitelist[newAddress].theAddress = true;
+        emit NewWallet(msg.sender, newAddress);
+        return true;
+    }
+
     function VestingTokenBalance() view public returns (uint) {
         return Lumerin.balanceOf(address(this));
     }
-    /*
-     Attempting alt method. Stoping this code as of a meeting on Jan 11, 2022
-    function addUser(address _addressToWhitelist) public onlyOwner {
-      whitelisted[_addressToWhitelist] = true;
-    }
-    */
+
     function TransferLumerin(address to, uint256 amount) public {
         require(msg.sender == owner, "Vesting Contract Owner can transfer Tokens, not you!"); 
         uint256 LumerinBalance = Lumerin.balanceOf(address(this));
