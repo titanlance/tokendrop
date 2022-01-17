f1d253d96bf3dc560423e0a9f200b27b31b14b00
Lance Seidman <83887474+titanlance@users.noreply.github.com>
2022-01-17 01:02:34 -0800

    Update Drop.sol
    Added onlyOwner for main Token transfer w/o Claim.

--- contracts/Drop.sol
+++ contracts/Drop.sol
@@ -7,8 +7,6 @@ pragma solidity ^0.8.0;
  *
  * @author Lance Seidman (Titan Mining/Lumerin Protocol)
 */
-
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract FastLumerinDrop {
     address public owner;
     mapping(address => uint) public people;
@@ -25,6 +23,13 @@ contract FastLumerinDrop {
     constructor() {
         owner = msg.sender;
     }
+    modifier onlyOwner() {
+      require(msg.sender == owner, "Sorry, only owner of this contract can perform this task!");
+      _;
+    }
+    receive() payable external {
+        emit TransferReceived(msg.sender, msg.value);
+    }    
     function addWallet (address walletAddr, uint _qty) public {
         people[walletAddr] = _qty;
     }
@@ -53,7 +58,7 @@ contract FastLumerinDrop {
             emit MSG('Not Whitelisted!');
         }
     } 
-    function TransferLumerin(address to, uint amount) public {
+    function TransferLumerin(address to, uint amount) public onlyOwner{
         require(msg.sender == owner, "Vesting Contract Owner can transfer Tokens, not you!"); 
         uint256 LumerinBalance = Lumerin.balanceOf(address(this));
         require(amount <= LumerinBalance, "Token balance is low!");
